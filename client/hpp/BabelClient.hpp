b1ac59166362dc08b5836d8e76c52ff411cf6c82
Guillaume GELIN <perso@ramnes.eu>
2011-11-22 19:30:54 +0100

    client: Makefile

---
+++ client/hpp/BabelClient.hpp
@@ -0,0 +1,21 @@
+//
+// BabelClient.hpp for Babel project
+// 
+// Made by ramnes
+// <perso@ramnes.eu>
+// 
+// Started on  Tue Nov 22 18:18:37 2011 by ramnes
+// Last update Tue Nov 22 18:24:31 2011 ramnes
+//
+
+#ifndef		__BABELCLIENT_HPP
+# define	__BABELCLIENT_HPP
+
+class		BabelClient
+{
+public:
+  BabelClient();
+  ~BabelClient();
+};
+
+#endif		// __BABELCLIENT_HPP
---
+++ client/src/BabelClient.cpp
@@ -0,0 +1,17 @@
+//
+// BabelClient.cpp for Babel project
+// 
+// Made by ramnes
+// <perso@ramnes.eu>
+// 
+// Started on  Tue Nov 22 19:23:34 2011 by ramnes
+// Last update Tue Nov 22 19:24:18 2011 ramnes
+//
+
+#include	"BabelClient.hpp"
+#include	<iostream>
+
+int		main()
+{
+  std::cout << "Je suis un ouf et je rox le babel en oneshot" << std::endl;
+}
