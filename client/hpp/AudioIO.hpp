2a80683b49225e7a949a82c5f11ddfd301ed9f30
Yann KOETH <yann.koeth@epitech.eu>
2011-11-24 13:33:32 +0100

    mise en place des tests AudioIO

---
+++ client/hpp/AudioIO.hpp
@@ -0,0 +1,19 @@
+//
+// AudioIO.hpp for Babel in /Users/yannkoeth/scm/Babel/client/hpp
+// 
+// Made by koeth_y
+// Login   <koeth_y@epitech.net>
+// 
+// Started on  Thu Nov 24 13:24:53 2011 koeth_y
+// Last update Thu Nov 24 13:28:01 2011 koeth_y
+//
+
+class AudioIO
+{
+public:
+  AudioIO();
+  
+  AudioIO(const AudioIO&);
+  AudioIO& operator=(const AudioIO&);
+  ~AudioIO();
+};
---
+++ client/src/AudioIO.cpp
@@ -0,0 +1,25 @@
+//
+// AudioIO.cpp for Babel in /Users/yannkoeth/scm/Babel/client/src
+// 
+// Made by koeth_y
+// Login   <koeth_y@epitech.net>
+// 
+// Started on  Thu Nov 24 13:28:48 2011 koeth_y
+// Last update Thu Nov 24 13:28:56 2011 koeth_y
+//
+
+#include <iostream>
+#include "AudioIO.hpp"
+
+AudioIO::AudioIO()
+{
+  std::cout << "AudioIO constructor" << std::endl;
+}
+
+AudioIO::~AudioIO()
+{
+	
+}
+
+
+
