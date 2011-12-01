79513f9e3f2007e50586750d8f289a9eb55e6873
Guillaume GELIN <perso@ramnes.eu>
2011-12-01 18:07:37 +0100

    Headers des Makefile

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,7 +5,7 @@
 // <perso@ramnes.eu>
 // 
 // Started on  Tue Nov 22 19:49:31 2011 by ramnes
-// Last update Thu Nov 24 12:08:28 2011 ramnes
+// Last update Thu Dec  1 18:04:25 2011 ramnes
 //
 
 #include	"BabelClient.hpp"
@@ -16,10 +16,9 @@ int		main()
 {
   BabelClient* client = new BabelClient();
 
-  if (!client->run())
-    return (EXIT_FAILURE);
+  client->run();
   client->loadConfig("autoexec.cfg");
-  if (!client->connectTo())
-    return (EXIT_FAILURE);
+  client->connectTo();
+  delete client;
   return (EXIT_SUCCESS);
 }
